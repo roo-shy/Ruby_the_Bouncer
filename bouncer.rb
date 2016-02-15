4a1ae80bd2849837200e43c029537743f29cfcce
Rooshy <ru_sha_nia@yahoo.ca>
2016-02-15 14:30:57 -0600

    Add edit

--- bouncer.rb
+++ bouncer.rb
@@ -7,17 +7,20 @@ end
 puts show_menu
 
 def check_age
-  puts "What is your age?"
-    result = gets.chomp.to_i
-  if result < 18
-    puts "You can't do anything!"
-  elsif result >= 18 && result <= 21
-    puts "You can vote and you can smoke"
-  elsif result >= 21 && result <=25
-    puts "You can vote, you can drink and smoke"
-  elsif result >= 25
-   puts "You can vote, drink and rent a car"
-  else quit
+entry = ""
+  while entry != "quit" do
+    puts "What is your age?"
+    entry = gets.chomp
+    result = entry.to_i
+    if result < 18
+      puts "You can't do anything!"
+    elsif result >= 18 && result < 21
+      puts "You can vote and you can smoke"
+    elsif result >= 21 && result < 25
+      puts "You can vote, you can drink and smoke"
+    elsif result >= 25
+     puts "You can vote, drink and rent a car"
+    end
   end
 end
 
